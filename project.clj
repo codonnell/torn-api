990d704fae93a0da82dbe3a92ae5459f93be6f69
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-11-13 14:19:52 -0500

    Push to 1.9.0-RC1

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Client for Torn API"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [clj-http "3.7.0"]
                  [cheshire "5.8.0"]])
